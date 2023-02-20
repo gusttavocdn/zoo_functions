5076cccdd538bd37af73d70013fee168561090ee
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-02-20 13:39:51 -0300

    feat: add function to calculate entry prices

--- src/calculateEntry.ts
+++ src/calculateEntry.ts
@@ -1,6 +1,6 @@
-import { species } from '../data/zoo_data';
+import { prices } from '../data/zoo_data';
 
-type Visitants = {
+type EntrantsByAgeGroup = {
   child: number
   adult: number
   senior: number
@@ -11,12 +11,26 @@ type Entrant = {
   age: number
 };
 
-const countEntrants = (entrants: Entrant[]): Visitants => {
-  return null as any;
-};
+function countEntrants(entrants: Entrant[]): EntrantsByAgeGroup {
+  const entrantsByAgeGroup: EntrantsByAgeGroup = entrants.reduce(
+    (visitants, currentValue) => {
+      if (currentValue.age < 18) visitants.child += 1;
+      if (currentValue.age >= 18 && currentValue.age < 50) visitants.adult += 1;
+      if (currentValue.age >= 50) visitants.senior += 1;
+      return visitants;
+    },
+    { adult: 0, child: 0, senior: 0 }
+  );
 
-const calculateEntry = (entrants: Entrant[]): Visitants => {
-  return null as any;
-};
+  return entrantsByAgeGroup;
+}
+
+function calculateEntry(entrants: Entrant[]): number {
+  const { adult, child, senior } = countEntrants(entrants);
+  const totalEntryValue =
+    adult * prices.adult + child * prices.child + senior * prices.senior;
+
+  return totalEntryValue;
+}
 
 export { calculateEntry, countEntrants };
